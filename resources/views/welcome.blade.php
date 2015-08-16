0bcf46d11a0946cf90e56c5d0a6b7998dcfb30e7
Nick Franciosi <nickfranciosi@gmail.com>
2015-08-16 12:03:33 -0500

    general setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,27 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
-
+<html><head>
+        <title>Nick Franciosi | Developer | Nashville, TN</title>
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
         <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
+        <link href="css/app.css" rel="stylesheet">
         <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
+            
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Nick Franciosi</div>
+                <div class="quote">full stack developer</div>
+                <p>Full Site Coming Soon</p>
+                <ul>
+                    <li><a href="mailto:nickfranciosi@gmail.com" target="_blank"><i class="fa fa-envelope-o"></i></a></li>
+                    <li><a href="//twitter.com/nickfranciosi" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="//github.com/nickfranciosi" target="_blank"><i class="fa fa-github"></i></a></li>
+                </ul>
             </div>
         </div>
-    </body>
-</html>
+
+        <script type="text/javascript" src="js/bundle.js"></script>
+    
+
+</body></html>
--- tests/BlogTest.php
+++ tests/BlogTest.php
@@ -4,16 +4,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class BlogTest extends TestCase
 {
     /**
      * A basic functional test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testPageIsUp()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $this->visit('/blog')
+             ->see('Blog');
     }
 }
